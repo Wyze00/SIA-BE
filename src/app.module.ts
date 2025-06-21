f4e5a97a77e87e345fd92028f8dda88cf8af8a48
Wyze00 <nathantanusubroto15@gmail.com>
2025-06-21 19:47:11 +0700

    generate auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-    imports: [CommonModule],
+    imports: [CommonModule, AuthModule],
 })
 export class AppModule {}
