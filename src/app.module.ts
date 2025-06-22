64b2220d0333c9f612901e08e747c67c8595fa13
Wyze00 <nathantanusubroto15@gmail.com>
2025-06-22 16:14:41 +0700

    generate module mahasiswa and dosen

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { MahasiswaModule } from './modules/mahasiswa/mahasiswa.module';
+import { DosenModule } from './modules/dosen/dosen.module';
 
 @Module({
-    imports: [CommonModule, AuthModule],
+    imports: [CommonModule, AuthModule, MahasiswaModule, DosenModule],
 })
 export class AppModule {}
