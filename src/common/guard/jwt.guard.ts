c8386e594178fae4c703abdd7e576efa9da036ba
Wyze00 <nathantanusubroto15@gmail.com>
2025-06-23 15:24:32 +0700

    update jwt with count user

--- src/common/guard/jwt.guard.ts
+++ src/common/guard/jwt.guard.ts
@@ -5,23 +5,22 @@ import {
     HttpStatus,
     Injectable,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
 import { RequestUser } from '../dto/request-user.dto';
 import { JwtService } from '@nestjs/jwt';
 import { UserRole } from '../dto/user-role.dto';
 import { ROLES_KEY } from '../decorator/roles.decorator';
 import { Reflector } from '@nestjs/core';
+import { PrismaService } from '../provider/prisma.service';
 
 @Injectable()
 export class JwtGuard implements CanActivate {
     constructor(
         private readonly jwtService: JwtService,
         private readonly reflector: Reflector,
+        private readonly prismaService: PrismaService,
     ) {}
 
-    canActivate(
-        context: ExecutionContext,
-    ): boolean | Promise<boolean> | Observable<boolean> {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
         const request: RequestUser = context.switchToHttp().getRequest();
         const token: string | undefined = request.headers.authorization;
         if (!token || !token.startsWith('Bearer ')) {
@@ -40,7 +39,14 @@ export class JwtGuard implements CanActivate {
             );
 
             if (!requiredRoles || requiredRoles.length === 0) return true;
-            return requiredRoles.includes(decoded.role);
+
+            const count: number = await this.prismaService.user.count({
+                where: {
+                    id: decoded.id,
+                },
+            });
+
+            return requiredRoles.includes(decoded.role) && count === 1;
         } catch (e: any) {
             throw new HttpException(
                 `Unauthorized: ${e}`,
